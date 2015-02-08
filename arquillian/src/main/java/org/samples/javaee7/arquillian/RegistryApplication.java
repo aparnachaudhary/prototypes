3332ff396f2278ac8b6aa42d1bf239438a7e7bca
Aparna Chaudhary <aparna.chaudhary@gmail.com>
2015-02-08 10:17:01 +0100

    Initial commit

---
+++ arquillian/src/main/java/org/samples/javaee7/arquillian/RegistryApplication.java
@@ -0,0 +1,9 @@
+package org.samples.javaee7.arquillian;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("registry")
+public class RegistryApplication extends Application {
+
+}
